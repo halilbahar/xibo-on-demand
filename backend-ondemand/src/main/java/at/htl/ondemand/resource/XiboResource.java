e14362b6656ba7b67b9fcf134f562b1ada3a1377
halilbahar <halil.s.bahar@gmail.com>
2021-06-02 20:39:22 +0200

    Begin schedule endpoint

--- backend-ondemand/src/main/java/at/htl/ondemand/resource/XiboResource.java
+++ backend-ondemand/src/main/java/at/htl/ondemand/resource/XiboResource.java
@@ -1,11 +1,15 @@
 package at.htl.ondemand.resource;
 
+import at.htl.ondemand.model.xibo.Display;
+import at.htl.ondemand.model.xibo.Media;
 import at.htl.ondemand.service.XiboService;
+import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -28,8 +32,36 @@ public class XiboResource {
     }
 
     @POST
-    @Path("schedule")
-    public Response scheduleVideo() {
-        return null;
+    @Path("schedule/{displayId}/{videoId}")
+    public Response scheduleVideo(@PathParam("displayId") Long displayId, @PathParam("videoId") Long videoId) {
+        // Check if display exists
+        List<Display> displays = this.xiboService.getDisplays();
+        boolean displayIdValid = false;
+        for (Display display : displays) {
+            if (display.displayId.equals(displayId)) {
+                displayIdValid = true;
+                break;
+            }
+        }
+        if (!displayIdValid) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // Check if video exists
+        boolean videoIdValid = false;
+        List<Media> videos = this.xiboService.getVideos();
+        for (Media video : videos) {
+            if (video.mediaId.equals(videoId)) {
+                videoIdValid = true;
+                break;
+            }
+        }
+        if (!videoIdValid) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // If everything is valid schedule
+
+        return Response.noContent().build();
     }
 }
