d7bb08d77cbb4f98bbd12a67d0f7f1bd2ad0bcde
halilbahar <halil.s.bahar@gmail.com>
2021-06-03 15:24:23 +0200

    Create toolbar

--- frontend-ondemand/src/app/app.module.ts
+++ frontend-ondemand/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigService } from './core/services/config.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MediaListComponent } from './pages/media-list/media-list.component';
 import { DisplayListComponent } from './pages/display-list/display-list.component';
+import { MediaListComponent } from './pages/media-list/media-list.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { DisplayListComponent } from './pages/display-list/display-list.componen
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [
     {
--- frontend-ondemand/src/app/pages/media-list/media-list.component.ts
+++ frontend-ondemand/src/app/pages/media-list/media-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-media-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.apiService.getVideos().subscribe(v => console.log(v));
   }
 
 }
